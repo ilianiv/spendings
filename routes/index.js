757f8e9529f22a7c830e03915c9bc4664799589e
ilianiv <ilian.iv@gmail.com>
2014-10-29 00:09:44 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Spendings app' });
+  res.render('index', {
+    title : 'Spendings app',
+    partials : {
+      layout : 'layout'
+    }
+  });
+});
+
+/* GET add spending page. */
+router.get('/add', function(req, res) {
+  res.render('add', {
+    title : 'Add new spending',
+    fields : [
+      {'field' : 'amount'},
+      {'field' : 'name'},
+      {'field' : 'date'}
+    ],
+    partials : {
+      layout : 'layout'
+    }
+  });
+});
+
+/* POST add spending page. */
+router.post('/add', function(req, res) { 
+  /* TO DO */
+  res.redirect('/add');
 });
 
 module.exports = router;
